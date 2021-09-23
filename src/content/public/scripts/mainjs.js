d42e75613000fbbe7d01def42e512a2d47fab121
root <root@DESKTOP-3GDTJKV.localdomain>
2021-09-23 15:17:33 -0500

    x

--- src/content/public/scripts/mainjs.js
+++ src/content/public/scripts/mainjs.js
@@ -1,4 +1,4 @@
-const socket = io.connect('https://06designs.com/chatbox/');
+const socket = io('https://06designs.com/chatbox/');
 
 const dom = {
     selId(elementId){ return(document.getElementById(elementId))  },
