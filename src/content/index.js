ae2e29da0fd6bd9f532acd3277438b72cb9ac4ca
root <root@DESKTOP-3GDTJKV.localdomain>
2021-09-23 14:32:12 -0500

    2

--- src/content/index.js
+++ src/content/index.js
@@ -14,16 +14,16 @@ const port = process.env.PORT;
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use('/chatbox', express.static(__dirname + 'public'));
+app.use('/chatbox', express.static('public'));
 
 app.get("/chatbox/", (req, res) => {
     console.log(__dirname)
-    res.render(__dirname + "/public/index");
+    res.render("/public/index");
 });
 
 app.post("/chatbox/", (req, res) => {
     req.body.display_name = sanitize(req.body.display_name);
-    res.render(__dirname + "/public", {userInfo: req.body});
+    res.render("/public", {userInfo: req.body});
 });
 
 http.listen(port, ()=>{
