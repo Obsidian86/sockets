da7a4e9db01e291d3ecf79d3ff643a905d13ed3c
root <root@DESKTOP-3GDTJKV.localdomain>
2021-09-23 12:31:23 -0500

    add dir to publix

--- src/content/index.js
+++ src/content/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT;
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/chatbox/public'));
 
 app.get("/chatbox/", (req, res) => {
     res.render(__dirname + "/public/index");
@@ -22,7 +22,7 @@ app.get("/chatbox/", (req, res) => {
 
 app.post("/chatbox/", (req, res) => {
     req.body.display_name = sanitize(req.body.display_name);
-    res.render(__dirname + "/public/chatbox", {userInfo: req.body});
+    res.render(__dirname + "/public", {userInfo: req.body});
 });
 
 http.listen(port, ()=>{ console.log("connected " + port); });
